4fa112106242a669622d0e47ef7788c48c8133c4
Drumstickz64 <gearman533@gmail.com>
2021-02-03 00:42:19 +0300

    made some minor tweaks

--- public/src.e31bb0bc.js
+++ public/src.e31bb0bc.js
@@ -1044,7 +1044,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38683" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51208" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
